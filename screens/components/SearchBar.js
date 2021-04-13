badd7975e62fa9ba12efdd02ceebe6db2b3b0e99
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-13 15:23:32 -0700

    Added a search icon to the search bar component

--- screens/components/SearchBar.js
+++ screens/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Feather name='search' size={30} />
       <Text>Search Bar</Text>
     </View>
   );
@@ -11,7 +13,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#F0EEEE',
+    backgroundColor: 'gray',
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15
