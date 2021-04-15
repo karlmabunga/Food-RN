b6a81d9744a566529c9540958dc0d426607de128
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-14 21:55:14 -0700

    There is now state in search screen being passed to search bar

--- screens/components/SearchBar.js
+++ screens/components/SearchBar.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange}) => {
   return (
-    <View style={styles.background}>
-      <Feather name='search' size={30} />
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+        style={styles.inputStyle}
+        placeholder='Search'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: 'gray',
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
   }
 });
 
