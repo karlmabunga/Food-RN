71c182ccac857bbe5956a421166f570b9ddfeca3
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-16 13:03:10 -0700

    Async function was created to get results for the API. Changed the text to show the state results length

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -7,11 +7,22 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
 
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose',
+      }
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => console.log('term was submitted')} />
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => searchApi()} />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
