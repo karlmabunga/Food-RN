13207e0d4933af66c341e89c398a4924bb6ca408
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-22 16:45:03 -0700

    Pictures are now able to render on the phone

--- screens/components/ResultsDetail.js
+++ screens/components/ResultsDetail.js
@@ -3,10 +3,19 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 
 const ResultsDetail = ({ result }) => {
   return (
-  <View>
-    <Text>{result.name}</Text>
-  </View>
+    <View>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text>{result.name}</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  }
+})
 
 export default ResultsDetail;
