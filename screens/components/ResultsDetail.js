af3d7d0db18054e0fdb96573e5cd32b15d07a094
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-22 17:03:29 -0700

    Changed the styling of the name and added the number of stars and reviews per business

--- screens/components/ResultsDetail.js
+++ screens/components/ResultsDetail.js
@@ -5,7 +5,8 @@ const ResultsDetail = ({ result }) => {
   return (
     <View>
       <Image style={styles.image} source={{ uri: result.image_url }} />
-      <Text>{result.name}</Text>
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
   );
 };
@@ -15,6 +16,9 @@ const styles = StyleSheet.create({
     width: 250,
     height: 120,
     borderRadius: 4
+  },
+  name: {
+    fontWeight: 'bold',
   }
 })
 
