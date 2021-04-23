1f9cd8486e874db19053760ef4b58aee2647b916
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-22 18:48:37 -0700

    Created a ResultsShowScreen for the navigation to navigate to when a user clicks on a restaurant. I've also added the screen to the navigator stack

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import 'react-native-gesture-handler';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './screens/SearchScreen';
+import ResultsShowScreen from './screens/ResultsShowScreen';
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
+  ResultsShow: ResultsShowScreen,
 }, {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
---
+++ screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsShowScreen = () => {
+  return (
+    <>
+      <Text>Results Show Screen</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
